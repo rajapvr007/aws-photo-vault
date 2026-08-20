04eacf9821e08650e992785e0b541accd20f8ce3
rajapvr007 <cse.raja007@gmail.com>
2026-08-20 10:17:17 +0530

    Prepare backend for cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
 const uploadRoutes = require("./routes/uploadRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/upload", uploadRoutes);
 
 app.get("/", (req, res) => {
@@ -20,13 +22,11 @@ app.get("/", (req, res) => {
 const imageRoutes = require("./routes/imageRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-    
-
 app.use("/images", imageRoutes);
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
